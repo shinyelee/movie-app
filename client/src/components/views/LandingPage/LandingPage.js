86f3030a89f9e7ad9a99f60b7612940e82fbf1b9
shinyelee <shinye0213@naver.com>
2021-02-18 21:47:07 +0900

    Landing Page 만들기 (1) 전체적인 Template을 간단하게 만들기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,38 +1,28 @@
-import React, { useEffect } from 'react'
-import axios from 'axios';
-import { withRouter } from 'react-router-dom'; 
+import React from 'react'
+// import { FaCode } from 'react-icons/fa';
+
 function LandingPage(props) {
 
-    useEffect(() => {
-        axios.get('/api/hello')
-            .then(response => { console.log(response) })
-    }, [])
-
-
-    const onClickHandler = () => {
-        axios.get(`/api/users/logout`)
-            .then(response => {
-                if (response.data.success) {
-                    props.history.push("/login")
-                } else {
-                    alert('로그아웃 하는데 실패 했습니다.')
-                }
-            })
-    }
-
     return (
-        <div style={{
-            display: 'flex', justifyContent: 'center', alignItems: 'center'
-            , width: '100%', height: '100vh'
-        }}>
-            <h2>시작 페이지</h2>
+        <div style={{ width: '100%', margin: '0' }}>
+            
+            {/* Main Image */}
 
-            <button onClick={onClickHandler}>
-                로그아웃
-            </button>
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* Movie Grid Cards */}
+
+            </div>
+
+            <div style={{display: 'flex', justifyContent: 'center '}}>
+                <button>Load More</button>
+            </div>
 
         </div>
     )
 }
 
-export default withRouter(LandingPage)
+export default LandingPage
