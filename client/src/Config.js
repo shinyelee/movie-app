99684ab630dc5b2031649d52313d470b81647a9a
shinyelee <shinye0213@naver.com>
2021-02-19 00:39:19 +0900

    Landing Page 만들기 (1) MainImage Component를 만들기

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,7 +1,6 @@
 // SERVER ROUTES
 export const USER_SERVER = '/api/users';
 
-
 export const API_URL = 'https://api.themoviedb.org/3/';
-
+export const API_KEY = 'df83cf7c4ded4540be93c361a9b13a8b';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,12 @@
-import { response } from 'express';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // import { FaCode } from "react-icons/fa";
-import { API_URL, API_KEY } from '../../Config';
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
 
     const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
 
     useEffect(() => {
         const endpoint = '${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1';
@@ -14,7 +15,10 @@ function LandingPage() {
         .then(response => response.json())
         .then(response => {
 
+            console.log(response)
+
             setMovies([response.results])
+            setMainMovieImage(response.results[0])
         })
         
     }, [])
@@ -24,18 +28,26 @@ function LandingPage() {
         <div style={{ width: '100%', margin: '0' }}>
             
             {/* Main Image */}
+            {MainMovieImage && 
+                <MainImage
+                    image={'${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}'}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+    
 
             <div style={{ width: '85%', margin: '1rem auto' }}>
 
-                <h2>Movies by latest</h2>
+                <h2> Movies by latest </h2>
                 <hr />
 
                 {/* Movie Grid Cards */}
 
             </div>
 
-            <div style={{display: 'flex', justifyContent: 'center '}}>
-                <button>Load More</button>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button> Load More </button>
             </div>
 
         </div>
