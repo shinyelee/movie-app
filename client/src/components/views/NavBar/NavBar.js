b4736f4051179c00c923394083b1c7ce3fde0634
shinyelee <shinye0213@naver.com>
2021-02-19 23:27:03 +0900

    NavBar.js 추가 수정 필요 NavBar.css 생성

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 // import LeftMenu from './Sections/LeftMenu';
 // import RightMenu from './Sections/RightMenu';
 import { Drawer, Button, Icon } from 'antd';
-// import './Sections/Navbar.css';
+import './Sections/Navbar.css';
 
 function NavBar() {
   const [visible, setVisible] = useState(false)
