0166a4f515c0cf7c7496f30f4d763c3dffca2923
shinyelee <shinye0213@naver.com>
2021-02-19 22:19:45 +0900

    Grid Card Component 만들기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 // import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../../Config';
 import MainImage from './Sections/MainImage';
+import axios from 'axios';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
 
@@ -9,17 +12,16 @@ function LandingPage() {
     const [MainMovieImage, setMainMovieImage] = useState(null)
 
     useEffect(() => {
-        const endpoint = '${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1';
-        
+
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
         fetch(endpoint)
-        .then(response => response.json())
-        .then(response => {
-
-            console.log(response)
-
-            setMovies([response.results])
-            setMainMovieImage(response.results[0])
-        })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response.results)
+                setMovies([...response.results])
+                setMainMovieImage(response.results[0])
+            })
         
     }, [])
 
@@ -30,7 +32,7 @@ function LandingPage() {
             {/* Main Image */}
             {MainMovieImage && 
                 <MainImage
-                    image={'${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}'}
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
                     title={MainMovieImage.original_title}
                     text={MainMovieImage.overview}
                 />
@@ -43,6 +45,21 @@ function LandingPage() {
                 <hr />
 
                 {/* Movie Grid Cards */}
+
+                <Row gutter={[16, 16]} >
+
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                image={movie.poster_path ?
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+
+                </Row>
 
             </div>
 
---
+++ client/src/components/views/commons/GridCards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Col } from 'antd';
+
+
+function GridCards() {
+    return (
+        <Col lg={6} md={8} xs={24}>
+            <div style={{ position: 'relative' }}>
+                <a href >
+                    <img />
+                </a>
+            </div>
+        </Col>
+    )
+}
+
+export default GridCards
