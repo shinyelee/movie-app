e7187d315e1d67501f2973158447fa6a17d43d6f
shinyelee <shinye0213@naver.com>
2021-02-20 14:03:36 +0900

    App.js 수정 -> LandingPage.js에 NavBar, Footer 등 표시됨

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { Suspense } from 'react';
+import { Route, Switch } from "react-router-dom";
 
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage';
@@ -15,24 +10,18 @@ import Auth from './hoc/auth';
 
 function App() {
   return (
-    <Router>
-      <div>
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null )  } />
-          <Route exact path="/login" component={Auth(LoginPage, false) } />
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
         </Switch>
       </div>
-    </Router>
+      <Footer />
+    </Suspense>
   );
 }
 
 export default App;
-
