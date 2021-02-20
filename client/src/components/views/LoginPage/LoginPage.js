6dc22dc9881040565d7d5d907cdb331f3e2164f8
shinyelee <shinye0213@naver.com>
2021-02-20 14:26:38 +0900

    auth.js 수정, user_action.js -> user_actions.js로 파일명 변경

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Axios from 'axios'
 import { useDispatch } from 'react-redux';
-import { loginUser } from '../../../_actions/user_action';
+import { loginUser } from '../../../_actions/user_actions';
 import { withRouter } from 'react-router-dom';
 function LoginPage(props) {
     const dispatch = useDispatch();
