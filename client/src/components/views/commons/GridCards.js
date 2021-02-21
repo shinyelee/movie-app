21e4d5ab6d8288b8e0f028cb443b62fb4cc180cc
shinyelee <shinye0213@naver.com>
2021-02-21 19:50:42 +0900

    GridCards.js 수정

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Col } from 'antd';
 
 
-function GridCards() {
+function GridCards(props) {
     return (
         <Col lg={6} md={8} xs={24}>
             <div style={{ position: 'relative' }}>
-                <a href >
-                    <img />
+                <a href={`/movie/${props.movieId}`} >
+                    <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName} />
                 </a>
             </div>
         </Col>
