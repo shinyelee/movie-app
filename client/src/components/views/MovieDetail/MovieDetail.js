20fbba5acf76da05194b8c6a3ece832710a9ec38
shinyelee <shinye0213@naver.com>
2021-02-23 14:40:24 +0900

    Favorite 버튼 만들기 (1) Model, Button UI 만들기

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -3,6 +3,7 @@ import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../../Config';
 import MainImage from '../../views/LandingPage/Sections/MainImage';
 import MovieInfo from './Sections/MovieInfo';
 import GridCards from '../commons/GridCards';
+import Favorite from './Sections/Favorite';
 import { Row } from 'antd';
 
 function MovieDetail(props) {
@@ -51,6 +52,10 @@ function MovieDetail(props) {
             {/* Body */}
 
             <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Favorite />
+                </div>
 
                 {/* Movie Info */}
 
