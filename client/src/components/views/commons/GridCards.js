48c9ada25db83152916078268e7525a39d99676c
shinyelee <shinye0213@naver.com>
2021-02-23 12:36:55 +0900

    영화 출연진들 가져오기 수정

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -15,15 +15,13 @@ function GridCards(props) {
                 </div>
             </Col>
         )
-        
+
     } else {
 
         return (
             <Col lg={6} md={8} xs={24}>
                 <div style={{ position: 'relative' }}>
-                    <a href={`/movie/${props.movieId}`} >
-                        <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.characterName} />
-                    </a>
+                    <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.characterName} />
                 </div>
             </Col>
         )
