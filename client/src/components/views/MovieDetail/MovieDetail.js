7a49b11525b5c9601a45781631c42dbcef22c519
shinyelee <shinye0213@naver.com>
2021-02-24 23:35:50 +0900

    Favorite list에 추가 삭제

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -4,7 +4,7 @@ import MainImage from '../../views/LandingPage/Sections/MainImage';
 import MovieInfo from './Sections/MovieInfo';
 import GridCards from '../commons/GridCards';
 import Favorite from './Sections/Favorite';
-import { Row } from 'antd';
+import { Button, Row } from 'antd';
 
 function MovieDetail(props) {
 
@@ -67,7 +67,7 @@ function MovieDetail(props) {
                 {/* Actors Grid */}
 
                 <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
-                    <button onClick={toggleActorView}> Toggle Actor View </button>
+                    <Button onClick={toggleActorView}> Toggle Actor View </Button>
                 </div>
 
                 {ActorToggle &&
--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios';
+import { Button } from 'antd';
+// import { response } from 'express';
 
 function Favorite(props) {
 
@@ -12,12 +14,15 @@ function Favorite(props) {
     const [FavoriteNumber, setFavoriteNumber] = useState(0)
     const [Favorited, setFavorited] = useState(false)
 
-    useEffect(() => {
+    let variables = {
+        userFrom: userFrom,
+        movieId: movieId,
+        movieTitle: movieTitle,
+        moviePost: moviePost,
+        movieRunTime: movieRunTime
+    }
 
-        let variables = {
-            userFrom,
-            movieId
-        }
+    useEffect(() => {
 
         Axios.post('/api/favorite/favoriteNumber', variables)
             .then(response => {
@@ -38,11 +43,38 @@ function Favorite(props) {
             })
     }, [])
 
+    const onClickFavorite = () => {
+
+        if(Favorited) {
+            Axios.post('/api/favorite/removeFromFavorite', variables)
+                .then(response => {
+                    if(response.data.success) {
+                        setFavoriteNumber(FavoriteNumber-1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite 리스트에서 삭제 실패했습니다.')
+                    }
+                })
+
+        } else {
+            Axios.post('/api/favorite/addToFavorite', variables)
+                .then(response => {
+                    if(response.data.success) {
+                        setFavoriteNumber(FavoriteNumber+1)
+                        setFavorited(!Favorited)
+                    } else {
+                        alert('Favorite 리스트에 추가 실패했습니다.')
+                    }
+                })
+        }
+    }
+
     return (
         <div>
-            <button> {Favorite ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber} </button>
+            <Button onClick={onClickFavorite}> {Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber} </Button>
         </div>
     )
+
 }
 
 export default Favorite
