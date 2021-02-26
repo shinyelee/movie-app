bc0903201d712c78e0deed2a82e5fb654efb5f46
shinyelee <shinye0213@naver.com>
2021-02-26 13:01:45 +0900

    Favorite 페이지 만들기 (1) MongoDB에서 Favorite 된 영화정보 가져오기

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './favorite.css';
+import Axios from 'axios';
 
 function FavoritePage() {
+
+    useEffect(() => {
+        
+        Axios.post('/api/favorite/getFavoredMovie', { userFrom: localStorage.getItem('userId') })
+            .then(response => {
+                if(response.data.success) {
+
+                } else {
+                    alert('영화 정보를 가져오는 데 실패했습니다.')
+                }
+            })
+    }, [])
+
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
             <h2> Favorite Movies </h2>
