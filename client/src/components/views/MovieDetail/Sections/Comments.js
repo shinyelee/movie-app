775a9388e250f9613c45f8d1d500434cf035196a
shinyelee <shinye0213@naver.com>
2021-03-02 16:25:03 +0900

    댓글 기능 생성 2 Comment js (1) template 만들기

--- client/src/components/views/MovieDetail/Sections/Comments.js
+++ client/src/components/views/MovieDetail/Sections/Comments.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState }  from 'react'
 
 function Comments() {
+
+    const [commentValue, setcommentValue] = useState("")
+
+    // const handleClick = (event) => {
+    //     setcommentValue(event.currentTarget.value)
+    // }
+
     return (
         <div>
-            Comments
+            <br />
+            <p> Replies </p>
+            <hr />
+
+            {/* Coment Lists */}
+
+            {/* Root Comment Form */}
+
+            <form style={{ display: 'flex' }} onSubmit>
+                <textarea
+                    style={{ width: '100%', borderRadius: '5px' }}
+                    // onChange={handleClick}
+                    value
+                    placeholder="댓글을 작성해 주세요"
+                />
+                <br />
+                <button style={{ width: '20%', height: '52px' }} onClick> Submit </button>
+            </form>
+
         </div>
     )
 }
